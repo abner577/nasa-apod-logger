27dbe5ffb732222557fe81fc6b2ece41e166b31b
Abner Rodriguez <abner07282005@gmail.com>
2026-03-14 12:40:38 -0400

    Remove wallpaper utils shim

--- src/utils/wallpaper_utils.py
+++
@@ -1,5 +0,0 @@
-"""Backward-compatible wallpaper utility exports for existing import paths."""
-
-from src.wallpaper import apply_auto_wallpaper_for_single_apod, apply_auto_wallpaper_from_file_path
-
-__all__ = ["apply_auto_wallpaper_for_single_apod", "apply_auto_wallpaper_from_file_path"]
